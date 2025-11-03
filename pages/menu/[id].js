2e8c4cb780afbaf7e8dbc30c854e7afae713ac3c
amir bagherzadeh dizaji <dizaji74@gmail.com>
2025-11-03 20:19:40 +0330

    Dynamic routes created.

---
+++ pages/menu/[id].js
@@ -0,0 +1,18 @@
+function Details() {
+  return <div></div>;
+}
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({ params: { id: food.id.toString() } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
